03d7198faa9e9976b4f3fda1c4a4794fca50718e
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2021-05-10 03:25:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Halic University";          
+            string strnew = " ";            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'c')
+                    strnew += 's';
+                else
+                    strnew += str[i];
+            }
+            Console.WriteLine(strnew);
+            //String str1 = str.Substring(6);
+            //Console.WriteLine(str1);
+            Console.ReadKey();
+            
+        }
+    }
+}
